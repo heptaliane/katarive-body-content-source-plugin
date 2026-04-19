08adcf4db01ccec41f5afc3e15e89beed52e06be
heptaliane <heptaliane@gmail.com>
2026-04-20 01:41:44 +0900

    test: Add comprehensive tests for GetSource method
    This commit introduces a new `TestGetSource` function to `main_test.go`.
    The test suite utilizes `httptest.NewServer` to mock an HTTP server, allowing for
    isolated and reliable testing of the `GetSource` method.
    
    The test cases cover various scenarios:
    - HTML content with no title
    - HTML content with title and text
    - HTML content including script tags
    - Non-HTML text content
    - Handling of a "not found" scenario
    
    These tests ensure the robustness and correctness of the `GetSource` method's
    behavior across different input types and error conditions.

--- main_test.go
+++ main_test.go
@@ -2,6 +2,10 @@ package main_test
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -32,4 +36,92 @@ func TestGetSourceServiceMetadata(t *testing.T) {
 		t.Errorf("Unmatched response: %v", diff)
 		return
 	}
+}
+
+func TestGetSource(t *testing.T) {
+	t.Parallel()
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data, err := os.ReadFile(fmt.Sprintf(".%s", r.URL.Path))
+		if err != nil {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+	}))
+
+	p := new(plugin.BodyContentSourceService)
+
+	cases := map[string]struct {
+		path             string
+		expectedResponse *pb.GetSourceResponse
+		isError          bool
+	}{
+		"no-title": {
+			path: "testdata/no-title.html",
+			expectedResponse: &pb.GetSourceResponse{
+				Content: "content",
+			},
+			isError: false,
+		},
+		"text-only": {
+			path: "testdata/text-only.html",
+			expectedResponse: &pb.GetSourceResponse{
+				Content: "content",
+				Title:   "title",
+			},
+			isError: false,
+		},
+		"with-script": {
+			path: "testdata/with-script.html",
+			expectedResponse: &pb.GetSourceResponse{
+				Content: "content",
+				Title:   "title",
+			},
+			isError: false,
+		},
+		"not-html": {
+			path: "testdata/not-html.txt",
+			expectedResponse: &pb.GetSourceResponse{
+				Content: "Not a html",
+			},
+			isError: false,
+		},
+		"not-found": {
+			path:    "testdata/not-found.html",
+			isError: true,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			req := &pb.GetSourceRequest{
+				Url: fmt.Sprintf("%s/%s", ts.URL, tc.path),
+			}
+			ctx := context.Background()
+			data, err := p.GetSource(ctx, req)
+			if tc.isError {
+				if err == nil {
+					t.Error("Error expected but got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+				return
+			}
+			if diff := cmp.Diff(data, tc.expectedResponse, protocmp.Transform()); diff != "" {
+				t.Errorf("Unmatched response: %v", diff)
+				return
+			}
+		})
+	}
+
+	t.Cleanup(func() {
+		ts.Close()
+	})
 }
